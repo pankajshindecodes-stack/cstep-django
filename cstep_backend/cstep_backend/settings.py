078d547f328f78a54a7e76b64c8c4314120122ee
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-06-09 19:48:00 +0530

    added logout api

--- cstep_backend/cstep_backend/settings.py
+++ cstep_backend/cstep_backend/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     # Third party
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "channels",
     # Local
