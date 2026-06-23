4ab5f7a8e5164aae0312d6fe341980e60d4d480c
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-06-23 12:13:02 +0530

    update static settings

--- cstep_backend/cstep_backend/urls.py
+++ cstep_backend/cstep_backend/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("analytics/", include("analytics.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
